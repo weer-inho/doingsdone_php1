fb72443cbaeec7ce2553983b92f3bb40ca1c235b
Руслан <60976203+weer-inho@users.noreply.github.com>
2023-03-16 14:13:33 +0300

    первая домашка

--- index.php
+++ index.php
@@ -71,8 +71,11 @@ $show_complete_tasks = rand(0, 1);
                     </nav>
 
                     <label class="checkbox">
-                        <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox">
+                        <input
+                            class="checkbox__input visually-hidden show_completed"
+                            type="checkbox"
+                            <?= $show_complete_tasks ? 'checked' : '' ?>
+                        >
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
                 </div>
@@ -92,7 +95,20 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
-                    <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
+                    <? if ($show_complete_tasks): ?>
+                        <tr class="tasks__item task task--completed">
+                            <td class="task__select">
+                                <label class="checkbox task__checkbox">
+                                    <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                    <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                                </label>
+                            </td>
+                            <td class="task__date">10.10.2019</td>
+
+                            <td class="task__controls">
+                            </td>
+                        </tr>
+                    <? endif;?>
                 </table>
             </main>
         </div>
