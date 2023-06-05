678ff3e90cc39250723fe5bd6696e850ff3ed010
Руслан <60976203+weer-inho@users.noreply.github.com>
2023-06-05 21:06:05 +0300

    добавление максимального уровня ошибок

--- data.php
+++ data.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+ini_set('error_log', __DIR__ . '/php-errors.log');
+
 session_start();
 $user_name = $_SESSION['name'] ?? null;
 $user_id   = $_SESSION['user_id'] ?? null;
